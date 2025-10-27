6149f35c863c5f0526b3fea88b87c5912c04c02e
MargineanMiruna-Elena <miruna.marginean.2018@gmail.com>
2025-10-27 21:28:09 +0200

    first commit

---
+++ backend/src/main/java/com/mme/travelassist/TravelassistApplication.java
@@ -0,0 +1,13 @@
+package com.mme.travelassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelassistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelassistApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mme/travelassist/TravelassistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mme.travelassist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelassistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
